d71ed9698b924edaed50784dbcb0ce884306b304
test-amz <113343383+test-amz@users.noreply.github.com>
2024-02-23 18:01:21 +0530

    Update main.py
    Made some changes to add AOS request

--- main.py
+++ main.py
@@ -4,6 +4,22 @@ from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
 from os import getenv
 
+import boto3
+import requests
+from requests_aws4auth import AWS4Auth
+
+host = 'https://search-test-sm-m5zxhdizelbznif3vjpfra56oe.us-east-1.es.amazonaws.com' 
+region = 'us-east-1'
+service = 'es'
+credentials = boto3.Session().get_credentials()
+awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+
+path = '/_search' # the OpenSearch API endpoint
+url = host + path
+
+headers = {"Content-Type": "application/json"}
+
+
 app = FastAPI(
     title="App Runner Demo Service",
     description="Demo API",
@@ -21,7 +37,8 @@ def root():
     msg = "Hello from app runner!"
     if DEMO_ENV_VAR:
         msg += f" You set this environment variable: {DEMO_ENV_VAR}"
-    return JSONResponse(status_code=status.HTTP_200_OK, content=msg)
+        r = requests.get(url, auth=awsauth, headers=headers)
+    return JSONResponse(status_code=status.HTTP_200_OK, content=r)
 
 if __name__ == '__main__':
     import uvicorn
